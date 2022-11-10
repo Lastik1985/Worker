70326a9d7a6e4e960abba83f0623ad4316ea6433
Lastik1985 <Lastik1985@mail.ru>
2022-11-11 01:17:55 +0300

    Worker

---
+++ src/Worker.java
@@ -0,0 +1,32 @@
+public class Worker {
+    private OnTaskDoneListener callback;
+    private OnTaskErrorListener errorCallback;
+
+    @FunctionalInterface
+    public interface OnTaskErrorListener {
+        void onError(String result);
+    }
+
+
+    @FunctionalInterface
+    public interface OnTaskDoneListener {
+        void onDone(String result);
+    }
+
+    protected Worker(OnTaskDoneListener callback, OnTaskErrorListener errorCallback) {
+        this.callback = callback;
+        this.errorCallback = errorCallback;
+    }
+
+    public void start() {
+        for (int i = 0; i < 100; i++) {
+            if (i + 1 == 33) {
+                errorCallback.onError(" Task " + (i + 1) + " is not done!");
+            } else {
+                callback.onDone(" Task " + (i + 1) + " is done ");
+            }
+        }
+
+    }
+
+}
